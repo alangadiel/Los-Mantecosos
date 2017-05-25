e234b9c96d972c2dd2baee76e57289887031fa7e
lucasmcenturion <lucasmcenturion@gmail.com>
2017-05-25 18:06:46 -0300

    funciones sin desarrollar pero interfaz hecha

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -1,9 +1,33 @@
-#include "SocketsL.h"
+#include <stdio.h>
+#include <commons/log.h>
+#include <string.h>
+#include <parser/metadata_program.h>
+#include "Primitivas.h"
 
 char* IP_KERNEL;
 int PUERTO_KERNEL;
 char* IP_MEMORIA;
 int PUERTO_MEMORIA;
+static const char* PROGRAMA =
+		"begin\n"
+		"variables a, b\n"
+		"a = 3\n"
+		"b = 5\n"
+		"a = b + 12\n"
+		"end\n"
+		"\n";
+
+AnSISOP_funciones functions = {
+  .AnSISOP_definirVariable = primitiva_definirVariable,
+  /*.AnSISOP_obtenerPosicionVariable= dummy_obtenerPosicionVariable,
+  .AnSISOP_finalizar     = dummy_finalizar,
+  .AnSISOP_dereferenciar   = dummy_dereferenciar,
+  .AnSISOP_asignar    = dummy_asignar,*/
+
+};
+
+
+AnSISOP_kernel kernel_functions = { };
 
 void obtenerValoresArchivoConfiguracion()
 {
@@ -44,7 +68,7 @@ void obtenerValoresArchivoConfiguracion()
 				{
 					char buffer[10000];
 
-					IP_MEMORIA = fgets(buffer, sizeof buffer, file);
+					IP_KERNEL = fgets(buffer, sizeof buffer, file);
 					strtok(IP_KERNEL, "\n");
 
 					contadorDeVariables++;
@@ -75,6 +99,28 @@ int main(void) {
 	obtenerValoresArchivoConfiguracion();
 	imprimirArchivoConfiguracion();
 	int socketKernel = ConectarAServidor(PUERTO_KERNEL, IP_KERNEL, KERNEL, CPU, RecibirHandshake);
-	int socketMEMORIA = ConectarAServidor(PUERTO_MEMORIA, IP_MEMORIA, MEMORIA, CPU, RecibirHandshake);
+	int socketMemoria = ConectarAServidor(PUERTO_MEMORIA, IP_MEMORIA, MEMORIA, CPU, RecibirHandshake);
+
+	Paquete* paquete = malloc(sizeof(Paquete));
+	uint32_t datosRecibidos = RecibirPaqueteCliente(socketKernel, CPU, paquete);
+	while(datosRecibidos<=0){
+		datosRecibidos = RecibirPaqueteCliente(socketKernel, CPU, paquete);
+	}
+	if (datosRecibidos > 0)
+	{
+		switch(paquete -> header.tipoMensaje)
+		{
+			case ESSTRING:
+				if (strcmp(paquete -> header.emisor, KERNEL) == 0)
+				{
+					printf("Texto recibido: %s", (char*)paquete -> Payload);
+					//analizadorLinea(PROGRAMA, AnSISOP_funciones, AnSISOP_funciones_kernel);
+				}
+			break;
+		}
+	}
+	//Hago free
+	free(paquete -> Payload);
+	free(paquete);
 	return 0;
 }
