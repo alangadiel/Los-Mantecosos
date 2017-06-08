7f230606ae306039360152835ec97dac03c5fcd1
martinsapo <martinsaposnic@gmail.com>
2017-06-08 17:39:53 -0300

    sigue fs

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -15,7 +15,6 @@ int TAMANIO_BLOQUES;
 int CANTIDAD_BLOQUES;
 char* MAGIC_NUMBER;
 int *bitmapArray;
-int YA_ESTA_CREADO;
 
 typedef struct {
 	int Tamanio;
@@ -68,19 +67,21 @@ void imprimirArchivoConfiguracion() {
 
 
 int validarArchivo(char* path, int socketFD) {
-	char fileToValidate = string_duplicate(PUNTO_MONTAJE);
+	char* fileToValidate = string_duplicate(PUNTO_MONTAJE);
 	strcat(fileToValidate, path);
-	if( access( fileToValidate, F_OK ) != -1 ) {
-	    if (socketFD != 0) {
-	    	EnviarDatos(socketFD,FS,1,sizeof(uint32_t));
-	    }
-	    return 1;
-	} else {
-		if (socketFD != 0) {
-			EnviarDatos(socketFD,FS,0,sizeof(uint32_t));
+	if (S_ISDIR(fileToValidate)) {
+		if( access( fileToValidate, F_OK ) != -1 ) {
+			if (socketFD != 0) {
+				EnviarDatos(socketFD,FS,(void*)1,sizeof(uint32_t));
+			}
+			return 1;
+		} else {
+			if (socketFD != 0) {
+				EnviarDatos(socketFD,FS,0,sizeof(uint32_t));
+			}
 		}
-		return 0;
 	}
+	return 0;
 }
 
 void cambiarValorBitmap(int posicion, int valor) {
@@ -88,12 +89,11 @@ void cambiarValorBitmap(int posicion, int valor) {
 	//cambiarlo en el archivo
 }
 
-int encontrarPrimerBloqueLibreYOcuparlo() {
+int encontrarPrimerBloqueLibre() {
 	int i = 0;
 	int seEncontro;
 	while (bitmapArray[i] < CANTIDAD_BLOQUES && seEncontro == 0) {
 		if (bitmapArray[i] == 0) {
-			cambiarValorBitmap(i, 1);
 			seEncontro = 1;
 		}
 	}
@@ -105,28 +105,30 @@ int encontrarPrimerBloqueLibreYOcuparlo() {
 	}
 }
 
-int* obtenerBloques(int bytes) {
-	int cantidadDeBloques = (int) ceil(bytes/TAMANIO_BLOQUES);
-	int* bloques;
+void crearBloques(int* bloques) {
 	int i;
-	if (cantidadDeBloques == 0) cantidadDeBloques = 1;
-	for (i = 0; i<cantidadDeBloques;i++) {
-		int posicion = encontrarPrimerBloqueLibreYOcuparlo();
-		if (posicion < 0) {
-			return -1;
-		}
-		char* newFile = string_duplicate(ARCHIVOSPATH);
-		strcat(newFile, string_itoa(posicion));
+	for (i = 0; i < length(bloques); i++) {
+		char* newFile = string_duplicate(BLOQUESPATH);
+		strcat(newFile, string_itoa(i));
 		strcat(newFile, ".bin");
 		FILE* file = fopen(newFile, "a");
-		bloques[i] = posicion;
+		fclose(file);
+		cambiarValorBitmap(bloques[i], 1);
 	}
-	i++;
-	bloques[i] = -1;
-	return bloques;
 }
 
-void modificarValoresDeArchivo(int tamanio, int bloques[], char* path) {
+void eliminarBloques(int bloques[]) {
+	int i;
+	for (i = 0; i < length(bloques); i++) {
+		cambiarValorBitmap(bloques[i], 0);
+		char* pathToRemove = string_duplicate(BLOQUESPATH);
+		strcat(pathToRemove, string_itoa(bloques[i]));
+		strcat(pathToRemove, ".bin");
+		remove(pathToRemove);
+	}
+}
+
+void modificarValoresDeArchivo(int tamanio, int* bloques, char* path) {
 	FILE* newFile = fopen(path, "a");
 	char* strTamanio = "TAMANIO=";
 	strcat(strTamanio, string_itoa(tamanio));
@@ -142,10 +144,27 @@ void modificarValoresDeArchivo(int tamanio, int bloques[], char* path) {
 	fclose(newFile);
 }
 
+int reservarBloques(int cantidadDeBloques, char* path, int size) {
+	int* bloques;
+	int i;
+	for (i = 0; i < cantidadDeBloques; i++) {
+		int posicion = encontrarPrimerBloqueLibre();
+		if (posicion < 0) {
+			return 0;
+		}
+		bloques[i] = posicion;
+	}
+	i++;
+	bloques[i] = -1;
+	crearBloques(bloques);
+	modificarValoresDeArchivo(size, bloques, path);
+	return 1;
+}
+
 void crearArchivo(char* path,int socketFD) {
 	char* pathForValidation = string_duplicate(ARCHIVOSPATH);
 	strcat(pathForValidation, path);
-	if (S_ISDIR(pathForValidation) && !validarArchivo(pathForValidation, 0)) {
+	if (!validarArchivo(pathForValidation, 0)) {
 		char** pathArray = string_split(path, "/");
 		int i = 0;
 		char* nuevoPath = string_duplicate(ARCHIVOSPATH);
@@ -157,10 +176,9 @@ void crearArchivo(char* path,int socketFD) {
 			i++;
 		}
 		strcat(nuevoPath, pathArray[i]);
-		int* bloques = obtenerBloques(0);
-		if (bloques >= 0) {
-			modificarValoresDeArchivo(0, bloques, nuevoPath);
-			EnviarDatos(socketFD,FS,1,sizeof(uint32_t));
+		int sePudoReservar = reservarBloques(1, nuevoPath, 0);
+		if (sePudoReservar) {
+			EnviarDatos(socketFD,FS,(void*)1,sizeof(uint32_t));
 		}
 		else {
 			EnviarDatos(socketFD,FS,0,sizeof(uint32_t));
@@ -168,24 +186,7 @@ void crearArchivo(char* path,int socketFD) {
 	}
 }
 
-void eliminarBloques(int bloques[]) {
-	int i;
-	for (i = 0; i < length(bloques); i++) {
-		char* pathToRemove = string_duplicate(BLOQUESPATH);
-		strcat(pathToRemove, string_itoa(bloques[i]));
-		strcat(pathToRemove, ".bin");
-		remove(pathToRemove);
-	}
-}
-
-void liberarBloquesBitmap(int bloques[]) {
-	int i;
-	for (i = 0; i < length(bloques); i++) {
-		cambiarValorBitmap(bloques[i], 0);
-	}
-}
-
-ValoresArchivo obtenerValoresDeArchivo(pathArchivo) {
+ValoresArchivo obtenerValoresDeArchivo(char* pathArchivo) {
 	ValoresArchivo valores;
 	return valores;
 }
@@ -196,10 +197,10 @@ void borrarArchivo(char* path, int socketFD) {
 	if (validarArchivo(pathArchivo, 0)) {
 		ValoresArchivo valores = obtenerValoresDeArchivo(pathArchivo);
 		eliminarBloques(valores.Bloques);
-		liberarBloquesBitmap(valores.Bloques);
+
 		int removeFile = remove(pathArchivo);
 		if (removeFile) {
-			EnviarDatos(socketFD,FS,1,sizeof(uint32_t));
+			EnviarDatos(socketFD,FS,(void*)1,sizeof(uint32_t));
 		}
 		else {
 			EnviarDatos(socketFD,FS,0,sizeof(uint32_t));
@@ -211,18 +212,69 @@ void borrarArchivo(char* path, int socketFD) {
 	}
 }
 
-void obtenerDatos(char* Path, uint32_t Offset, uint32_t Size, int socketFD) {
-	/*Ante un pedido de datos el File System devolverá del path enviado por parámetro, la
-cantidad de bytes definidos por el Size a partir del offset solicitado. Requiere que el archivo
-se encuentre abierto en modo lectura (“r”).
-En caso de que se soliciten datos o se intenten guardar datos en un archivo inexistente el File System
-deberá retornar un error de Archivo no encontrado.
-
-	 *
-	 */
+char* leerTodoElArchivo(FILE* file) {
+	fseek(file, 0, SEEK_END);
+	long size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	char* fcontent = malloc(size);
+	fread(fcontent, 1, size, file);
+	return fcontent;
 }
 
-void guardarDatos(char* Path, uint32_t Offset, uint32_t Size, uint32_t Buffer, int socketFD){
+char* obtenerTodosLosDatosDeBloques(int bloques[]) {
+	char* datos = string_new();
+	int i;
+	for (i = 0; i < length(bloques); i++) {
+		char* pathAlBloque = string_duplicate(BLOQUESPATH);
+		strcat(pathAlBloque, string_itoa(i));
+		strcat(pathAlBloque, ".bin");
+		strcat(datos, leerTodoElArchivo(pathAlBloque));
+	}
+	return datos;
+}
+
+void obtenerDatos(char* path, uint32_t offset, uint32_t size, int socketFD) {
+	char* pathAObtener = string_duplicate(ARCHIVOSPATH);
+	strcat(pathAObtener, path);
+	if (!validarArchivo(pathAObtener, 0)) {
+		ValoresArchivo valores = obtenerValoresDeArchivo(pathAObtener);
+		char* datos = obtenerTodosLosDatosDeBloques(valores.Bloques);
+		char* datosAEnviar = string_substring(datos, offset, size);
+		EnviarDatos(socketFD,FS,datosAEnviar,sizeof(char) * string_length(datosAEnviar));
+	}
+	else {
+		EnviarDatos(socketFD,FS,0,sizeof(uint32_t));
+	}
+}
+
+
+int obtenerTamanioDeArchivo(char* path) {
+	FILE* file = fopen(path, "r");
+	fseek(file, 0L, SEEK_END);
+	int tamanio = ftell(file);
+	fclose(file);
+	return tamanio;
+}
+
+int obtenerSobranteDelUltimoBloque(int bloque) {
+	char* pathUltimoBloque = string_duplicate(BLOQUESPATH);
+	strcat(pathUltimoBloque, string_itoa(bloque));
+	strcat(pathUltimoBloque, ".bin");
+	return TAMANIO_BLOQUES - obtenerTamanioDeArchivo(pathUltimoBloque);
+}
+
+void guardarDatos(char* path, uint32_t offset, uint32_t size, uint32_t buffer, int socketFD){
+	char* pathAEscribir = string_duplicate(ARCHIVOSPATH);
+	strcat(pathAEscribir, path);
+	if (!validarArchivo(pathAEscribir, 0)) {
+		ValoresArchivo valores = obtenerValoresDeArchivo(pathAEscribir);
+		int nuevoTamanioDeArchivoQueSeNecesita = valores.Tamanio + size;
+		int cantidadDeBloquesTotales = (int) ceil(nuevoTamanioDeArchivoQueSeNecesita/TAMANIO_BLOQUES);
+		int cantidadDeBloquesNecesitados = cantidadDeBloquesTotales - length(valores.Bloques);
+		reservarBloques(cantidadDeBloquesNecesitados, pathAEscribir, nuevoTamanioDeArchivoQueSeNecesita);
+		int restanteDelUltimoBloque = obtenerSobranteDelUltimoBloque(valores.Bloques[lenght(valores.Bloques) - 1]);
+
+	}
 	/*Ante un pedido de escritura el File System almacenará en el path enviado por parámetro, los
 bytes del buffer, definidos por el valor del Size y a partir del offset solicitado. Requiere que el
 archivo se encuentre abierto en modo escritura (“w”).
@@ -244,7 +296,7 @@ int RecibirPaqueteFileSystem (int socketFD, char receptor[11], Paquete* paquete)
 				paquete.header.tipoMensaje = ESHANDSHAKE;
 				paquete.header.tamPayload = sizeof(uint32_t);
 				strcpy(paquete.header.emisor, FS);
-				paquete.Payload=1;
+				paquete.Payload = (void*)1;
 				EnviarPaquete(socketFD, &paquete);
 			}
 			else {
@@ -299,11 +351,6 @@ void archivoMetadata() {
 				while ((c = getc(file)) != EOF)
 					if (c == '=')
 					{
-						if (contadorDeVariables == 1)
-						{
-							fscanf(file, "%i", &YA_ESTA_CREADO);
-							contadorDeVariables++;
-						}
 						if (contadorDeVariables == 2)
 						{
 							char buffer[10000];
@@ -347,14 +394,7 @@ void archivoMetadata() {
 	}
 }
 
-char* leerTodoElArchivo(FILE* file) {
-	fseek(file, 0, SEEK_END);
-	long size = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	char* fcontent = malloc(size);
-	fread(fcontent, 1, size, file);
-	return fcontent;
-}
+
 
 void archivoBitmap() {
 	int i;
@@ -421,36 +461,12 @@ int contarArchivosEnDirectorio() {
 	return file_count;
 }
 
-void crearArchivosVacios() {
-	int i;
-	int cantidadDeArchivos = contarArchivosEnDirectorio(ARCHIVOSPATH);
-	int faltante = CANTIDAD_BLOQUES - cantidadDeArchivos;
-	if (faltante < 0) {
-		for(i=cantidadDeArchivos-1;i>=CANTIDAD_BLOQUES;i--){
-			char* archivoAEliminar = string_duplicate(ARCHIVOSPATH);
-			strcat(archivoAEliminar, string_itoa(i));
-			strcat(archivoAEliminar, ".bin");
-			remove(archivoAEliminar);
-		}
-	}
-	else if (faltante > 0) {
-		for(i=cantidadDeArchivos;i<CANTIDAD_BLOQUES;i++){
-			char* nombre = string_duplicate(ARCHIVOSPATH);
-			strcat(nombre, string_itoa(i));
-			strcat(nombre, ".bin");
-			FILE* archivo = fopen(nombre, "a");
-			fclose(archivo);
-		}
-	}
-}
-
 int main(void) {
 	obtenerValoresArchivoConfiguracion();
 	imprimirArchivoConfiguracion();
 	crearEstructurasDeCarpetas();
 	archivoMetadata();
 	archivoBitmap();
-	crearArchivosVacios();
 	Servidor(IP, PUERTO, MEMORIA, accion, RecibirPaqueteFileSystem);
 	return 0;
 }
