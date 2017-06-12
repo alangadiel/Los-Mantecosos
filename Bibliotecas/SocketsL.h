1ccaced4ae7781e7985c2916adb36b5c71ff1be8
alangadiel <alangadiel@gmail.com>
2017-06-12 20:36:20 -0300

    movida la fun reciv handshacke de memoria a sockets porque cpu tambien la usa

--- Bibliotecas/SocketsL.h
+++ Bibliotecas/SocketsL.h
@@ -78,6 +78,7 @@ void EnviarPaquete(int socketCliente, Paquete* paquete);
 void EnviarMensaje(int socketFD, char* msg, char emisor[11]);
 
 void RecibirHandshake(int socketFD, char emisor[11]);
+uint32_t RecibirHandshake_DeMemoria(int socketFD, char emisor[11]); //Retorna el tamanio de pagina
 int RecibirDatos(void* paquete, int socketFD, uint32_t cantARecibir);
 int RecibirPaqueteServidor(int socketFD, char receptor[11], Paquete* paquete); //Responde al recibir un Handshake
 int RecibirPaqueteCliente(int socketFD, char receptor[11], Paquete* paquete); //No responde los Handshakes
