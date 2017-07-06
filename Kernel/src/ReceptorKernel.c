bdeb2de7f443613fdc4b94ea26a289c57934c0b7
ulukacher <uri.lukacher@gmail.com>
2017-07-06 19:48:29 -0300

    e

--- Kernel/src/ReceptorKernel.c
+++ Kernel/src/ReceptorKernel.c
@@ -319,6 +319,7 @@ void receptorKernel(Paquete* paquete, int socketConectado){
 				//termino la rafaga normalmente sin bloquearse
 				if(strcmp(paquete->header.emisor, CPU) == 0)
 				{
+					printf("Llego el pcb\n");
 					DatosCPU* cpuActual = list_find(CPUsConectadas, LAMBDA(bool _(void* item) { return ((DatosCPU*) item)->socketCPU == socketConectado; }));
 					BloqueControlProceso* pcb = list_find(Ejecutando->elements, LAMBDA(bool _(void* item) { return ((BloqueControlProceso*) item)->PID == cpuActual->pid; }));
 					if(pcb!=NULL){
@@ -334,6 +335,8 @@ void receptorKernel(Paquete* paquete, int socketConectado){
 							FinalizarPrograma(pcb->PID, FINALIZACIONNORMAL);
 						}
 						else if(pcb->ExitCode==STACKOVERFLOW){
+							printf("Entro al if de stackoverflow\n");
+
 							FinalizarPrograma(pcb->PID, STACKOVERFLOW);
 						}
 						else
