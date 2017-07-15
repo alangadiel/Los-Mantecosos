29f04c2d3af445fc23562a71b065f449c33a5f23
alangadiel <alangadiel@gmail.com>
2017-07-15 02:24:17 -0300

    carpetas fuera

--- Consola/Debug/src/Consola.d
+++
@@ -1,8 +0,0 @@
-src/Consola.d: ../src/Consola.c ../../Bibliotecas/SocketsL.h \
- ../../Bibliotecas/Helper.h ../../Bibliotecas/Helper.h
-
-../../Bibliotecas/SocketsL.h:
-
-../../Bibliotecas/Helper.h:
-
-../../Bibliotecas/Helper.h:
