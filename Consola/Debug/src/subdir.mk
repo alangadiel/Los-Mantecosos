11bae7ceb3882f84ce25885e1e86efd90ec3cd6f
alangadiel <alangadiel@gmail.com>
2017-07-14 23:22:42 -0300

    makefile

--- Consola/Debug/src/subdir.mk
+++ Consola/Debug/src/subdir.mk
@@ -23,21 +23,21 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-1c-Los-Mantecosos/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"../../Bibliotecas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/Helper.o: /home/utnso/workspace/tp-2017-1c-Los-Mantecosos/Bibliotecas/Helper.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-1c-Los-Mantecosos/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"../../Bibliotecas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/SocketsL.o: /home/utnso/workspace/tp-2017-1c-Los-Mantecosos/Bibliotecas/SocketsL.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-1c-Los-Mantecosos/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"../../Bibliotecas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
