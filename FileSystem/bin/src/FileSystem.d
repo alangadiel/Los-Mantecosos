f1b448844bb432b598ae2169cf3c92e568ad54ae
alangadiel <alangadiel@gmail.com>
2017-07-15 05:08:49 -0300

    bin

--- FileSystem/bin/src/FileSystem.d
+++
@@ -1,6 +0,0 @@
-src/FileSystem.d: ../src/FileSystem.c ../../Bibliotecas/SocketsL.h \
- ../../Bibliotecas/Helper.h
-
-../../Bibliotecas/SocketsL.h:
-
-../../Bibliotecas/Helper.h:
--- Kernel/bin/src/Service.d
+++
@@ -1,8 +0,0 @@
-src/Service.d: ../src/Service.c ../src/Service.h \
- ../../Bibliotecas/SocketsL.h ../../Bibliotecas/Helper.h
-
-../src/Service.h:
-
-../../Bibliotecas/SocketsL.h:
-
-../../Bibliotecas/Helper.h:
